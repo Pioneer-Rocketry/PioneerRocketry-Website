e007a4b987ff5771e79880abc907ff38d24a6918
Kris <kris.adams3000@gmail.com>
2024-07-18 13:24:29 -0500

    adjusted the aut decoder

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,34 +1,39 @@
 //decode JWT
 function decodeJWT(token) {
-    try {
+  try {
+    sJWT = token;
+    var headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[0]));
+    var payloadObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(sJWT.split(".")[1]));
 
-      const decodedPayload = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(token));
+    var decodedPayload = {
+      header: headerObj,
+      payload: payloadObj,
+    };
 
-      return decodedPayload;
-    } catch (error) {
-      console.error('Error decoding JWT:', error);
-      return null;
-    }
+    return decodedPayload;
+  } catch (error) {
+    console.error("Error decoding JWT:", error);
+    return null;
   }
-
+}
 
 // Google Sign In
 function onSignIn(googleUser) {
-    console.log("Signed in: ", googleUser);
-    
-    let id_token =decodeJWT(googleUser.credentials);
-    console.log(id_token);
-    fetch('https://google-auth.kris-adams3000.workers.dev/googleRedirect', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id_token: id_token })
+  console.log("Signed in: ", googleUser);
+
+  let id_token = decodeJWT(googleUser.credentials);
+  console.log(id_token);
+  fetch("https://google-auth.kris-adams3000.workers.dev/googleRedirect", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id_token: id_token }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Handle authenticated user
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        // Handle authenticated user
-    })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 }
