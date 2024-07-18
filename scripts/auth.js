58a02c6bf8ff6e3bba6f8b5b4bd97f7877da31a8
Kris <kris.adams3000@gmail.com>
2024-07-18 10:05:23 -0500

    added simple google auth

---
+++ scripts/auth.js
@@ -0,0 +1,17 @@
+// Google Sign In
+function onSignIn(googleUser) {
+    let id_token = googleUser.getAuthResponse().id_token;
+    fetch('https://google-auth.kris-adams3000.workers.dev/googleRedirect', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id_token: id_token })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Handle authenticated user
+    })
+    .catch(error => console.error('Error:', error));
+}
