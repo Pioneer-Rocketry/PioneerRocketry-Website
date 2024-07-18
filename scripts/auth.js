e38d287b96b22b7341501b22a880555f667b1e47
Kris <kris.adams3000@gmail.com>
2024-07-18 13:15:48 -0500

    debugging

--- scripts/auth.js
+++ scripts/auth.js
@@ -21,8 +21,9 @@ function decodeJWT(token) {
 // Google Sign In
 function onSignIn(googleUser) {
     console.log("Signed in: ", googleUser);
-
-    let id_token = googleUser.getAuthResponse().id_token;
+    
+    let id_token =decodeJWT(googleUser.credentials);
+    console.log(id_token);
     fetch('https://google-auth.kris-adams3000.workers.dev/googleRedirect', {
         method: 'POST',
         headers: {
